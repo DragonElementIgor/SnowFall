682239240dff7efe0303e35b07f4c10426f348ce
DragonElementIgor <116569057+DragonElementIgor@users.noreply.github.com>
2023-01-21 22:35:11 +0500

    Realese0.1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ pygame.display.set_caption("New Year!")
 inWindow = True
 clock = pygame.time.Clock()
 fps = 14
-
+bigSnows = False
 
 
 snows = []
@@ -20,7 +20,7 @@ class Snow:
     def __init__(self, width):
         super().__init__()
         self.snowSize = randint(32, 40)
-        self.y = 0 - randint(self.snowSize, self.snowSize+20)
+        self.y = 0 - randint(self.snowSize, self.snowSize+15)
         self.image = pygame.transform.scale(pygame.image.load("snow.png").convert_alpha(), (self.snowSize, self.snowSize))
         self.x = randrange(0, width - self.snowSize, 13)
         self.speed = randint(10, 18)
@@ -29,20 +29,21 @@ class Snow:
 
     def draw(self, sc):
         sc.blit(self.image, (self.x, self.y))
-    def move(self, height, width, snows, fps):
+    def move(self, height, width, snows, fps, bigSnows):
         xchanche = randint(-3, 3)
         self.timeCount += 1
         self.x += xchanche
         if self.timeCount >= fps * 6 and self.tochedToGround:
             self.y += self.speed
-        elif self.y > height:
+        if self.y >= height or self.x < 0 or self.x > width:
             snows.remove(self)
         else:
             if self.y > (height - self.snowSize):
                 self.y -= 4
                 if not self.tochedToGround:
-                    for i in range(2):
-                        snows.append(Snow(width))
+                    if not bigSnows:
+                        for i in range(2):
+                            snows.append(Snow(width))
                     self.tochedToGround = True
             else:
                 self.y += self.speed
@@ -56,11 +57,16 @@ for i in range(35):
 while inWindow:
     sc.fill((0, 0, 0))
     sc.blit(bg, (0, 0))
+    if len(snows) >= 260:
+        bigSnows = True
+    if bigSnows:
+        if len(snows) < 120:
+            bigSnows = False
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             inWindow = False
     for snow in snows:
-        snows = snow.move(height, width, snows, fps)
+        snows = snow.move(height, width, snows, fps, bigSnows)
         snow.draw(sc)
 
 
